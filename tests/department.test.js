45abafc94c12a5937239e11251f1f39ebd71c9b5
Sandip Gyawali <sandipgyawali10@gmail.com>
2024-01-17 20:39:12 +0545

    added the edit label test for the department-page

--- tests/department.test.js
+++ tests/department.test.js
@@ -38,4 +38,41 @@ describe("Department", () => {
     if (data != "test-data")
       throw new Error("The input new department is not added");
   });
+
+  it("edit-department existing dept", async () => {
+    // get the last element of the table body
+    const lastElement = await driver.findElement(By.css("tbody tr:last-child"));
+
+    // edit btn target
+    const editBtnWrapper = await lastElement.findElement(
+      By.css("td:nth-child(3)")
+    );
+
+    const editBtn = await editBtnWrapper.findElement(By.css("a"));
+
+    await driver.executeScript("arguments[0].click();", editBtn);
+
+    // now change the input
+    const input = await driver.findElement(By.id("departmentName"));
+
+    await input.clear();
+    await input.sendKeys("test-data-modified");
+
+    // submit
+    await driver
+      .findElement(By.className("common-court-btn btn btn-success"))
+      .click();
+
+    // now check if the element is modified or not
+    const modifiedElement = await driver.findElement(
+      By.css("tbody tr:last-child")
+    );
+
+    const newData = await modifiedElement
+      .findElement(By.css("td:nth-child(2)"))
+      .getText();
+
+    if (newData != "test-data-modified")
+      throw new Error("The department input was not able to be edited.");
+  });
 });
