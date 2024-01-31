084025904309def05ae8ac68a23b3e05b9ab2e3e
Sandip Gyawali <sandipgyawali10@gmail.com>
2024-01-31 11:40:52 +0545

    uncommented the previos test case

--- tests/userRole.test.js
+++ tests/userRole.test.js
@@ -20,73 +20,73 @@ describe("user-role", () => {
     await driver.quit();
   });
 
-  // it("user-role-add-test 201", async () => {
-  //   // first login to the page before starting the test
-  //   await driver.get("https://demo.ejalas.com/login");
+  it("user-role-add-test 201", async () => {
+    // first login to the page before starting the test
+    await driver.get("https://demo.ejalas.com/login");
 
-  //   await login(driver);
+    await login(driver);
 
-  //   await driver.get("https://demo.ejalas.com/admin/role");
+    await driver.get("https://demo.ejalas.com/admin/role");
 
-  //   // add the input tag enter name of role and submit
-  //   const inputELement = await driver.findElement(By.id("name"));
+    // add the input tag enter name of role and submit
+    const inputELement = await driver.findElement(By.id("name"));
 
-  //   await inputELement.sendKeys(role);
+    await inputELement.sendKeys(role);
 
-  //   // submit
-  //   await driver
-  //     .findElement(By.className("common-court-btn btn btn-success"))
-  //     .click();
+    // submit
+    await driver
+      .findElement(By.className("common-court-btn btn btn-success"))
+      .click();
 
-  //   // check at the last of the table body if the role has been added or not
-  //   const lastElement = await driver.wait(
-  //     until.elementLocated(By.css("tbody tr:last-child"))
-  //   );
+    // check at the last of the table body if the role has been added or not
+    const lastElement = await driver.wait(
+      until.elementLocated(By.css("tbody tr:last-child"))
+    );
 
-  //   // now check if the user input role name is matched
-  //   const data = await lastElement
-  //     .findElement(By.css(`td:nth-child(2)`))
-  //     .getText();
+    // now check if the user input role name is matched
+    const data = await lastElement
+      .findElement(By.css(`td:nth-child(2)`))
+      .getText();
 
-  //   if (data != role) throw new Error("The user role is not added.");
-  // });
+    if (data != role) throw new Error("The user role is not added.");
+  });
 
-  // // edit user role.
-  // it("user-role-test-edit 202", async () => {
-  //   await driver.get("https://demo.ejalas.com/admin/role");
+  // edit user role.
+  it("user-role-test-edit 202", async () => {
+    await driver.get("https://demo.ejalas.com/admin/role");
 
-  //   // edit the role
-  //   const element = await driver.findElement(By.css("tbody tr:last-child"));
+    // edit the role
+    const element = await driver.findElement(By.css("tbody tr:last-child"));
 
-  //   const editButtonWrapper = await element.findElement(
-  //     By.css("td:nth-child(3)")
-  //   );
+    const editButtonWrapper = await element.findElement(
+      By.css("td:nth-child(3)")
+    );
 
-  //   const editBtn = await editButtonWrapper.findElement(By.css(`a`));
+    const editBtn = await editButtonWrapper.findElement(By.css(`a`));
 
-  //   await driver.executeScript("arguments[0].click();", editBtn);
+    await driver.executeScript("arguments[0].click();", editBtn);
 
-  //   // target the input field and edit accordingly
-  //   const input = await driver.findElement(By.id("name"));
+    // target the input field and edit accordingly
+    const input = await driver.findElement(By.id("name"));
 
-  //   await input.clear();
+    await input.clear();
 
-  //   await input.sendKeys(role_edit);
+    await input.sendKeys(role_edit);
 
-  //   // now submit the change
-  //   const button = await driver.findElement(By.css("button"));
+    // now submit the change
+    const button = await driver.findElement(By.css("button"));
 
-  //   await driver.executeScript("arguments[0].click();", button);
+    await driver.executeScript("arguments[0].click();", button);
 
-  //   // now check if the edited section has been reflected or not
-  //   const lastElement = await driver.findElement(By.css("tbody tr:last-child"));
+    // now check if the edited section has been reflected or not
+    const lastElement = await driver.findElement(By.css("tbody tr:last-child"));
 
-  //   const text = await lastElement
-  //     .findElement(By.css("td:nth-child(2)"))
-  //     .getText();
+    const text = await lastElement
+      .findElement(By.css("td:nth-child(2)"))
+      .getText();
 
-  //   if (text != role_edit) throw new Error("The user role is not updated");
-  // });
+    if (text != role_edit) throw new Error("The user role is not updated");
+  });
 
   it("permission-management module-addition 203", async () => {
     await driver.get("https://demo.ejalas.com/login");
